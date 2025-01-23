8ca2d32e2d671a723c3ec7e94664fa77548a1710
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2025-01-23 20:56:12 +0530

    update

---
+++ Library Management System/issue_books.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Library_Management_System
+{
+    partial class issue_books
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tet_studentname = new System.Windows.Forms.TextBox();
+            this.tet_studentdept = new System.Windows.Forms.TextBox();
+            this.tet_studentsem = new System.Windows.Forms.TextBox();
+            this.tet_studentcontact = new System.Windows.Forms.TextBox();
+            this.tet_studentemail = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tet_enrollment = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.tet_enrollment);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.tet_studentemail);
+            this.panel1.Controls.Add(this.tet_studentcontact);
+            this.panel1.Controls.Add(this.tet_studentsem);
+            this.panel1.Controls.Add(this.tet_studentdept);
+            this.panel1.Controls.Add(this.tet_studentname);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(47, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1044, 431);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Enrollment No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(522, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(533, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Student Dept";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(526, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Student Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(554, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Issue Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(533, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Student Sem";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(506, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Student Contact";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(74, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search Student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tet_studentname
+            // 
+            this.tet_studentname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_studentname.Location = new System.Drawing.Point(661, 39);
+            this.tet_studentname.Name = "tet_studentname";
+            this.tet_studentname.Size = new System.Drawing.Size(321, 30);
+            this.tet_studentname.TabIndex = 2;
+            // 
+            // tet_studentdept
+            // 
+            this.tet_studentdept.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_studentdept.Location = new System.Drawing.Point(661, 86);
+            this.tet_studentdept.Name = "tet_studentdept";
+            this.tet_studentdept.Size = new System.Drawing.Size(321, 30);
+            this.tet_studentdept.TabIndex = 3;
+            // 
+            // tet_studentsem
+            // 
+            this.tet_studentsem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_studentsem.Location = new System.Drawing.Point(661, 129);
+            this.tet_studentsem.Name = "tet_studentsem";
+            this.tet_studentsem.Size = new System.Drawing.Size(321, 30);
+            this.tet_studentsem.TabIndex = 4;
+            // 
+            // tet_studentcontact
+            // 
+            this.tet_studentcontact.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_studentcontact.Location = new System.Drawing.Point(661, 176);
+            this.tet_studentcontact.Name = "tet_studentcontact";
+            this.tet_studentcontact.Size = new System.Drawing.Size(321, 30);
+            this.tet_studentcontact.TabIndex = 5;
+            // 
+            // tet_studentemail
+            // 
+            this.tet_studentemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_studentemail.Location = new System.Drawing.Point(661, 220);
+            this.tet_studentemail.Name = "tet_studentemail";
+            this.tet_studentemail.Size = new System.Drawing.Size(321, 30);
+            this.tet_studentemail.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(661, 260);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(321, 30);
+            this.textBox6.TabIndex = 7;
+            // 
+            // tet_enrollment
+            // 
+            this.tet_enrollment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tet_enrollment.Location = new System.Drawing.Point(32, 89);
+            this.tet_enrollment.Name = "tet_enrollment";
+            this.tet_enrollment.Size = new System.Drawing.Size(328, 30);
+            this.tet_enrollment.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(535, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(124, 25);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Books Name";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(661, 311);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(321, 30);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Issue Books";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // issue_books
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 489);
+            this.Controls.Add(this.panel1);
+            this.Name = "issue_books";
+            this.Text = "issue_books";
+            this.Load += new System.EventHandler(this.issue_books_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tet_enrollment;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox tet_studentemail;
+        private System.Windows.Forms.TextBox tet_studentcontact;
+        private System.Windows.Forms.TextBox tet_studentsem;
+        private System.Windows.Forms.TextBox tet_studentdept;
+        private System.Windows.Forms.TextBox tet_studentname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Library Management System/issue_books.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Library_Management_System
+{
+    public partial class issue_books : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=library_managemant_system;Integrated Security=True;");
+
+        public issue_books()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select  from student_info where student_enrollment_no '" + tet_enrollment.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if(i==0 )
+            {
+                MessageBox.Show("This.enrollment no not found");
+            }
+            else
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    tet_studentname.Text = dr["student_name"].ToString();
+                    tet_studentdept.Text = dr["student_department"].ToString();
+                    tet_studentsem.Text = dr["student_sem"].ToString();
+                    tet_studentcontact.Text = dr["student_contact"].ToString();
+                    tet_studentemail.Text = dr["student_email"].ToString();
+                }
+            }
+        }
+
+        private void issue_books_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+        }
+    }
+}
